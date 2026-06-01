2c2f6d78e78a58d62323298dc96807c78257e2b2
Cássio Oliveira <cassioc945@gmail.com>
2026-06-01 16:11:17 -0300

    Add files via upload

--- src/script.js
+++ src/script.js
@@ -204,18 +204,18 @@ const USUARIOS_PERFIS = {
         senha: "23082007",
         nome: "Cassio Oliveira",
         links: [
-            { texto: "👤 Meus Animais", url: "../../pages/user-pages/tutor.html" },
-            { texto: "🛠️ Minhas Solicitações", url: "../../pages/user-pages/handout.html" },
-            { texto: "📅 Favoritos", url: "../../pages/user-pages/favoritos.html" }
+            { texto: "👤 Meus Animais", url: "../../pages/usuarios/tutor.html" },
+            { texto: "🛠️ Minhas Solicitações", url: "../../pages/usuarios/handout.html" },
+            { texto: "📅 Favoritos", url: "../../pages/usuarios/favoritos.html" }
         ]
     },
     "tutor": {
         senha: "123456",
         nome: "Tutor de Animais",
         links: [
-            { texto: "👤 Meus Animais", url: "../../pages/user-pages/tutor.html" },
-            { texto: "🛠️ Minhas Solicitações", url: "../../pages/user-pages/handout.html" },
-            { texto: "📅 Favoritos", url: "../../pages/user-pages/favoritos.html" }
+            { texto: "👤 Meus Animais", url: "../../pages/usuarios/tutor.html" },
+            { texto: "🛠️ Minhas Solicitações", url: "../../pages/usuarios/handout.html" },
+            { texto: "📅 Favoritos", url: "../../pages/usuarioss/favoritos.html" }
         ]
     },
     "candidato": {
@@ -223,26 +223,26 @@ const USUARIOS_PERFIS = {
         nome: "Candidato a Adotante",
         links: [
             { texto: "🐾 Animais Disponíveis", url: "../../pages/adocao/index.html" },
-            { texto: "📝 Formulário de Interesse", url: "../../pages/user-pages/form_savor.html" },
-            { texto: "ℹ️ Status do Processo", url: "../../pages/user-pages/processo.html" }
+            { texto: "📝 Formulário de Interesse", url: "../../pages/usuarios/form_savor.html" },
+            { texto: "ℹ️ Status do Processo", url: "../../pages/usuarios/processo.html" }
         ]
     },
     "ong": {
         senha: "ong$-135",
         nome: "ONG Parceira",
         links: [
-            { texto: "🐾 Cadastrar Animal", url: "../../pages/user-pages/favoritos.html" },
-            { texto: "📊 Relatórios de Adoção", url: "../../pages/user-pages/favoritos.html" },
-            { texto: "🤝 Solicitar Apoio", url: "../../pages/user-pages/favoritos.html" }
+            { texto: "🐾 Cadastrar Animal", url: "../../pages/usuarios/favoritos.html" },
+            { texto: "📊 Relatórios de Adoção", url: "../../pages/usuarios/favoritos.html" },
+            { texto: "🤝 Solicitar Apoio", url: "../../pages/usuarios/favoritos.html" }
         ]
     },
     "prefeitura": {
         senha: "pref@456",
         nome: "Gestão Prefeitura",
         links: [
-            { texto: "📈 Painel de Castrações", url: "../../pages/user-pages/favoritos.html" },
-            { texto: "🚨 Gerenciar Denúncias", url: "../../pages/user-pages/favoritos.html" },
-            { texto: "🏢 Alocação de Recursos", url: "../../pages/user-pages/favoritos.html" }
+            { texto: "📈 Painel de Castrações", url: "../../pages/usuarios/favoritos.html" },
+            { texto: "🚨 Gerenciar Denúncias", url: "../../pages/usuarios/favoritos.html" },
+            { texto: "🏢 Alocação de Recursos", url: "../../pages/usuarios/favoritos.html" }
         ]
     }
 };
